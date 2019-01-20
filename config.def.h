35dff0492ea4840b52e3b19aaf0f1cda7bf65027
jstn <pogue.justin@gmail.com>
2019-01-19 22:47:54 -0500

    too transparent

--- config.def.h
+++ config.def.h
@@ -83,7 +83,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-unsigned int alpha = 0xc0;
+unsigned int alpha = 0xd0;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
